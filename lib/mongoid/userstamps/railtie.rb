6392ccbd221b8d0556c99bf67ebeaa5f9eacacbc
Geoffroy Planquart <geoffroy@planquart.fr>
2017-02-01 18:23:44 +0100

    using before_action instead of before_filter

--- lib/mongoid/userstamps/railtie.rb
+++ lib/mongoid/userstamps/railtie.rb
@@ -5,7 +5,7 @@ module Mongoid
     class Railtie < Rails::Railtie
       # Set current_user from controller reader method
       ActiveSupport.on_load :action_controller do
-        before_filter do |c|
+        before_action do |c|
           Mongoid::Userstamps::Config.user_classes.each do |user_class|
             begin
               user_class.current = c.public_send(user_class.userstamps_user.reader)
